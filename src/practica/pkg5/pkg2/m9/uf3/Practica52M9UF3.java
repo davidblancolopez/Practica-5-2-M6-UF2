aaa7a6ee473ab814db4b9f851ebf65d32552f5e5
davidblancolopez <daviddbl94@gmail.com>
2017-03-15 18:47:14 +0100

    CAMBIOS.

--- src/practica/pkg5/pkg2/m9/uf3/Practica52M9UF3.java
+++ src/practica/pkg5/pkg2/m9/uf3/Practica52M9UF3.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica.pkg5.pkg2.m9.uf3;
 
-/**
- *
- * @author ALUMNEDAM
- */
+
 public class Practica52M9UF3 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
